ab5db101b455a3bbf13dbde9ceabf49176996acb
Emily Harber <emily.a.harber@gmail.com>
2019-09-25 01:46:28 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -6,5 +6,21 @@ class Book
   def title 
     @title 
   end 
+  
+  def author=(author)
+    @author = author 
+  end 
+  
+  def author 
+    @author 
+  end 
+  
+  def page_count=(num)
+    @page_count = num 
+  end 
+  
+  def page_count
+    @page_count
+  end 
 end 
 
